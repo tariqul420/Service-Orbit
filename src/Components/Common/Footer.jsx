0da7b20a361580d8d61c78c42e367ad28c20f0ee
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-25 08:54:47 +0600

    update footer

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,16 +1,18 @@
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF, FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <div className="pt-4 w-full bg-gray-100 dark:bg-gray-700/30">
             <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10 mb-8">
                 <div className="space-y-4">
-                    <div className="flex items-center gap-2">
-                        <img src="./service-logo.png" alt="Logo" className="w-12" />
-                        <h2 className="font-extrabold text-2xl">Service Orbit</h2>
-                    </div>
+                    <Link to='/'>
+                        <div className="flex items-center gap-2">
+                            <img src="./service-logo.png" alt="Logo" className="w-12" />
+                            <h2 className="font-extrabold text-2xl">Service Orbit</h2>
+                        </div>
+                    </Link>
                     <p className="font-medium text-xl">Subscribe</p>
                     <div>
                         <p className="font-medium mb-2">Get 10% off your order</p>
@@ -37,9 +39,8 @@ const Footer = () => {
                 <div className="space-y-4">
                     <h2 className="font-semibold text-2xl">Support</h2>
                     <p>My account</p>
-                    <p>Login/Register</p>
-                    <p>Services</p>
-                    <p>Dashboard</p>
+                    <p><Link to='/login'>Login</Link>/<Link to='/register'>Register</Link></p>
+                    <p><Link to='/services'>Services</Link></p>
                 </div>
                 <div className="space-y-4">
                     <h2 className="font-semibold text-2xl">Quick Link</h2>
@@ -60,14 +61,14 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="flex items-center justify-between mt-10">
-                        <FaFacebookF size={25} />
-                        <FaXTwitter size={25} />
-                        <FaInstagram size={25} />
-                        <FaLinkedinIn size={25} />
+                        <a href="https://www.facebook.com/tariqul.islam.fb" target="_blank"><FaFacebookF size={25} /></a>
+                        <a href="https://github.com/tariqul420" target="_blank"><FaGithub size={25} /></a>
+                        <a href="https://www.instagram.com/tariqul.islam.fb" target="_blank"><FaInstagram size={25} /></a>
+                        <a href="https://www.linkedin.com/in/tariqul420" target="_blank"><FaLinkedinIn size={25} /></a>
                     </div>
                 </div>
             </div>
-            <p className="dark:text-white/70 text-center py-4 border-t border-solid border-color-primary-d dark:border-white">&copy; Copyright FilmFusion {new Date().getFullYear()}. All Right Reserved</p>
+            <p className="dark:text-white/70 text-center py-4 border-t border-solid border-color-primary-d dark:border-white">&copy; Copyright <Link to='/'>Service_Orbit</Link> {new Date().getFullYear()}. All Right Reserved</p>
         </div>
     );
 };
